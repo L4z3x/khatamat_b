b93d4abfbff01e74921bc365635e3dd448e89dc9
L4z3x <moussaousselmal1970@gmail.com>
2024-10-29 20:56:34 +0100

    updated notification models

--- notification/models.py
+++ notification/models.py
@@ -2,39 +2,48 @@ from django.db import models
 from khatma.models import khatmaGroup,Khatma
 from api.models import MyUser
 from django.utils import timezone
+from community.models import community
 
 
 
 class Notification(models.Model):
-    TYPES= [
-        ("join request","join request"),
-    ]
     STATUS = [
         ("read","read"),
          ("unread","unread")
     ]
-
-    owner = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=False,related_name="notification_from")
-    user = models.ForeignKey(MyUser,on_delete=models.SET_NULL,null=True,default=None,related_name="notification_to")
-    kh_G = models.ForeignKey(khatmaGroup,on_delete=models.SET_NULL,null=True,default=None)
-    Kh = models.ForeignKey(Khatma,on_delete=models.SET_NULL,null=True,default=None)
-    type = models.CharField(max_length=100,choices=TYPES)
-    message = models.CharField(max_length=100,null=True)
+    owner = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=False,related_name="notification")
     status = models.CharField(max_length=10,choices=STATUS)
     created_at = models.DateTimeField(timezone.now)
 
+    class Meta:
+        abstract = True
 
-    def __str__(self):
-        return f"N:{self.type} for {self.owner}"
 
-
-class joinRequest(models.Model):
-    owner = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=False,related_name="JoinRequest_owner")
-    user = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=False)
-    khatmaGroup = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE,null=False)
-    created_at = models.DateTimeField(default=timezone.now)
+class joinRequest(Notification):
+    user = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=False,related_name="JoinRequest_owner")
+    community = models.ForeignKey(community,on_delete=models.CASCADE,null=False)    
     
     class Meta:
-        unique_together =  [("user","khatmaGroup","owner")]
+        unique_together =  [("user","community","owner")]
+   
     def __str__(self):
-        return f" R:from {self.user} to {self.owner} in {self.khatmaGroup}"
+        return f" R:from {self.user} to {self.owner} in {self.community}"
+
+
+class message(Notification):
+    # message = models.ForeignKey()
+    khatmaGroup = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.message} in {self.khatmaGroup}'
+
+
+class postN(Notification):
+    post = models.OneToOneField('joinRequest')
+    
+    def __str__(self) -> str:
+        return f'notification of {self.post}'
+    
+
+# class invitationN(Notification):
+#     invitation = models.ForeignKey(invitation)
