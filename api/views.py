c0aa71285749df569a496378f666a0fc67470b06
L4z3x <moussaousselmal1970@gmail.com>
2024-11-04 23:35:26 +0100

    deleted updateUserapi and updated mutual_brother

--- api/views.py
+++ api/views.py
@@ -4,10 +4,11 @@ from rest_framework import status
 from .serializers import *
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
-
+from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import IsAuthenticated,AllowAny
 
-class ListUserapi(generics.ListAPIView,generics.RetrieveAPIView):
+@extend_schema(operation_id="get_user_list")
+class ListUserapi(generics.ListAPIView,generics.RetrieveAPIView): # remove in production
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
@@ -19,7 +20,7 @@ class ListUserapi(generics.ListAPIView,generics.RetrieveAPIView):
         else:
             return self.list(request)
 
-
+@extend_schema(operation_id="create_user_account")
 class CreateUserapi(generics.CreateAPIView):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
@@ -28,16 +29,7 @@ class CreateUserapi(generics.CreateAPIView):
     def post(self,request):
         return self.create(request)
 
-
-class UpdateUserapi(): # set it manually cause it doesn't work
-    queryset = MyUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def put(self,request):
-        return self.partial_update(request,request.user.pk)
-
-
+@extend_schema(operation_id="delete_user_account")
 class DeleteUserapi(generics.DestroyAPIView):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
@@ -53,49 +45,58 @@ class DeleteUserapi(generics.DestroyAPIView):
         else:
             return Response('No id specified',status=status. HTTP_400_BAD_REQUEST)
 
-
+@extend_schema(
+    operation_id="get_user_brother_list",
+    summary="get brother list"
+)
 class brother(views.APIView):
     permission_classes = [IsAuthenticated]
-    lookup_field = 'id'
-    def get(self,request,id): # get brothers
+    
+    def get(self,request,**args): # get brothers
         user = request.user
+        id = self.kwargs.get('id')
+        print(id)
         if id:
-            brother = MyUser.objects.get(id=id)
-            if user.private == True: # add private to user model default is False
-                if not user in brother.brothers:
-                    return Response(data={"msg":"private accounte"},status=status.HTTP_302_FOUND)
-            brothers = brother.brothers.all() | brother.brothers_set.all() 
+            brother = MyUser.objects.filter(id=id).first()
+            
+            # add private to user model default is False
+            if brother.private == True and user not in brother.brothers.all() and user not in brother.brothers_set.all():
+                return Response(data={"msg":"private accounte"},status=status.HTTP_302_FOUND) 
+
+            brothers = brother.brothers.all().union(brother.brothers_set.all())
         else:
-            brothers = user.brothers.all() | user.brothers_set.all()
-        print(brothers)
+            brothers = user.brothers.all().union(user.brothers_set.all())
+
         serializer = brotherDataSer(brothers,many=True).data
-        print(serializer)
+
         if serializer:
             return Response(serializer,status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    def post(self,request,format=None):
-        user = request.user
-        pass
 
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+@extend_schema(responses=brotherDataSer,operation_id="update_user")
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def updateUser(request):
     user = request.user
-    data = request.data
-    print(data)
-    if data:
-        for key, value in data.items():
-            if hasattr(user,key):
-                setattr(user, key, value)
-        if 'profilePic' in request.FILES:
-            user.profilePic = request.FILES['profilePic']
-        user.save()
-        return Response(status=status.HTTP_202_ACCEPTED,data={"msg":"updated succesfully"})
-    return Response(status=status.HTTP_400_BAD_REQUEST,data={"msg":"no data"})
+
+    serializer = UserSerializer(instance=user, data=request.data, partial=True)
+    if serializer.is_valid():
+        print(serializer.validated_data)    
+
+    if not serializer.is_valid():
+        return Response(status=status.HTTP_400_BAD_REQUEST,data=serializer.errors)
+    
+    serializer.save()
+    
+   
+    return Response(status=status.HTTP_202_ACCEPTED,data={"msg":"updated succesfully"})
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
+@extend_schema(operation_id="delete_brother")
 def deleteBrother(request,id): # remove a brother from your list
     user = request.user
     try:
@@ -110,48 +111,61 @@ def deleteBrother(request,id): # remove a brother from your list
     return Response(status=status.HTTP_200_OK,data={"msg":f"{brother.username} removed"})
 
 
-def blockBrother(request): # block a brother
-    pass
-
+@extend_schema(responses=brotherDataSer,operation_id="get_mutual_brothers")
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def mutualBrother(request,id): #get mutual friends if account is not private
+def mutualBrother(request, id): # get mutual brother of a brother
     user = request.user
     try:
         brother = MyUser.objects.get(id=id)
-    except Exception as e: # remove in production
-        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":f"{e}"}) 
+    except MyUser.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={"error": "Brother not found"})
     
-    br_brother_1 = brother.brothership_initiated.all()
-    br_brother_2 = brother.brothership_received.all()
-    br_user_1 = user.brothership_initiated.all()
-    br_user_2 = user.brothership_received.all()
-    match = []
-    # matching algorithm 
-    for br1 in br_brother_1:
-        for br2 in br_user_1:
-            if br2.user2 == br1.user2 and br2.user2 != brother and br1.user2 != user:
-                match.append(br1.user2)
-        for br2 in br_user_2:
-            if br2.user1 == br1.user2 and br2.user1 != brother and br1.user2 != user:
-                match.append(br1.user2)
-    for br1 in br_brother_2:
-        for br2 in br_user_1:
-            if br2.user2 == br1.user1 and br2.user2 != brother and br1.user1 != user:
-                match.append(br1.user1)
-        for br2 in br_user_2:
-            if br2.user1 == br1.user1 and br2.user1 != brother and br1.user1 != user:
-                match.append(br1.user1)
-    if not match:
-        return Response(status=status.HTTP_404_NOT_FOUND,data={"msg":"no mutual brothers"})    
-    serializer = brotherDataSer(match,many=True).data
-    return Response(status=status.HTTP_200_OK,data=serializer)
+    user_brothers = set(user.brothers.all())
+    brother_brothers = set(brother.brothers.all())
+    mutual_brothers = user_brothers.intersection(brother_brothers)
+    
+    mutual_brothers.discard(brother)
+    mutual_brothers.discard(user)
+    if not mutual_brothers:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={"msg": "No mutual brothers"})
+    
+    serializer = brotherDataSer(mutual_brothers, many=True).data
+    return Response(status=status.HTTP_200_OK, data=serializer)
 
-    # print(brBr)
-    # print(brBr2)
-    # print(br_brother)
-    # print(br_user)
-    # for br1 in br_brother:
-        # for br2 in br_user:
-            # if br2.user1 == br1.user1 or br2.user1 == br1.user2
- 
+@extend_schema(responses=brotherDataSer,operation_id="get_blocked_list")
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_blocked(request):
+    user = request.user
+    blocked = user.blocked.all()
+    if not blocked:
+        return Response(status=status.HTTP_200_OK,data={})
+    data = []
+    for block in blocked:
+        serializer = brotherDataSer(block).data
+        data.append(serializer)
+    return Response(status=status.HTTP_200_OK,data=data)
+
+@api_view(['POST'])
+@extend_schema(operation_id="block_brother")
+@permission_classes([IsAuthenticated])
+def blockBrother(request,id): # block a brother
+    user = request.user
+
+    try:
+        blocked = MyUser.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"msg":"no id associated with that Id"})
+
+    # check for friends to delete
+    if user.brothers.filter(id=blocked.id).exists():
+        user.brothers.remove(blocked)
+    # check for friends to delete
+    if user.brothers_set.filter(id=blocked.id).exists():
+        user.brothers_set.remove(blocked)
+
+    user.blocked.add(blocked)
+    user.save()
+    
+    return Response(status=status.HTTP_202_ACCEPTED)
