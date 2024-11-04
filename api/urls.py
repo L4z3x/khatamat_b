94278b31578d52014b0dd698211a120e3021fc2d
L4z3x <moussaousselmal1970@gmail.com>
2024-11-04 23:27:42 +0100

    added 3 urls

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path("retreive/<int:id>/", views.ListUserapi.as_view(), name="retreive-user"),
     path("retreive-all/", views.ListUserapi.as_view(), name="retreive-all-users"), # to be removed in production
     path("update/", views.updateUser , name="update-user"),
-    path("list-brother/<int:id>/",views.brother.as_view(),name="get user's brothers"),
+    path("list-brother/<int:id>/",views.brother.as_view(),name="get a user's brothers"),
+    path("list-brother/",views.brother.as_view(),name="get the user's brothers"),
     path("delete-brother/<int:id>/",views.deleteBrother ,name="delete brother from brother List"),
     path("mutual-brother/<int:id>/",views.mutualBrother ,name="ger mutuals brothers"),
+    path("list-blocked/",views.list_blocked ,name="list blocked brothers"),
+    path("block-brother/<int:id>/",views.blockBrother ,name="blocke brother"),
+    
 ]
