4700b91a54020b247b00b40d3103cab53e0c79ae
L4z3x <moussaousselmal1970@gmail.com>
2024-11-04 23:31:43 +0100

    updated retreive url

--- khatma/urls.py
+++ khatma/urls.py
@@ -3,7 +3,7 @@ from khatma import views
 
 urlpatterns = [
     path("create/",views.CreateKhatma.as_view(),name="create khatma"),
-    path("retreive/",views.getkhatma.as_view(),name="retreive khatma"),
+    path("retreive/<str:kh_name>/",views.getkhatma.as_view(),name="retreive khatma"),
     path("create-khatma-group/",views.CreateKhatmaGroup.as_view(),name="create khatma group"),
     path("add-member-kg/",views.khatma_G_memb.as_view(),name="add member to khatmaGroup"),
     path("add-member-k/",views.khatma_memb.as_view(),name="add member to khatma"),
