5f8e814eddd8315eedf3e587a580c258b085771e
L4z3x <moussaousselmal1970@gmail.com>
2024-11-05 22:15:51 +0100

    added a script to run the docker compose

--- build_container.sh
+++ build_container.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-d="1"
-command -v docker >/dev/null || d="0"
-if [[ "$d" == "0" ]];then
-	echo 'docker is not installed'
-else
-	docker build -t khatamat:latest .
-	docker run khatamat:latest
-fi
+
+# you should have docker and docker compose installed .
+
+docker compose up
+
 
