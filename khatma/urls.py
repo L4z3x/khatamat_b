01f43622a2fba74ff65b4db5ce23e7a46740ac81
L4z3x <moussaousselmal1970@gmail.com>
2024-11-08 00:05:01 +0100

    added auth for chat groups (uuid)

--- khatma/urls.py
+++ khatma/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from khatma import views
-
+from django_channels_jwt.views import AsgiValidateTokenView
 urlpatterns = [
+    path('auth/',AsgiValidateTokenView.as_view(),name='get uuid ticket'),
     path("create/",views.CreateKhatma.as_view(),name="create khatma"),
     path("retreive/<str:kh_name>/",views.getkhatma.as_view(),name="retreive khatma"),
     path("create-khatma-group/",views.CreateKhatmaGroup.as_view(),name="create khatma group"),
