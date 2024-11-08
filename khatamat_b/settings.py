827262a528dc0e9a5274ff25fcd9baba38c0d220
L4z3x <moussaousselmal1970@gmail.com>
2024-11-09 00:42:22 +0100

    added configuration for channels

--- khatamat_b/settings.py
+++ khatamat_b/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'daphne',
     'channels',
+    'channels_auth_token_middlewares',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
