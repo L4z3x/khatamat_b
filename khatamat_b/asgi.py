965d2d0199f8a7d706eb2f6ba350c8dabd9fc441
L4z3x <moussaousselmal1970@gmail.com>
2024-11-09 00:41:37 +0100

    added websocket to application

--- khatamat_b/asgi.py
+++ khatamat_b/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
+from django_channels_jwt.middleware import JwtAuthMiddlewareStack
+from channels_auth_token_middlewares.middleware import QueryStringSimpleJWTAuthTokenMiddleware
+import khatma.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khatamat_b.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # change to uuid method for more security : to be rethinked ...
+    # uuid => JwtAuthMiddlewareStack
+    # we use this just for rapid connections 
+    'websocket': QueryStringSimpleJWTAuthTokenMiddleware(
+        URLRouter(khatma.routing.urlpatterns)
+    )
+})
