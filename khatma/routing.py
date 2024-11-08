ea7226a218acfeb26e6ac5bf1b003a74e322107a
L4z3x <moussaousselmal1970@gmail.com>
2024-11-09 00:42:59 +0100

    added routing for websocket

--- khatma/routing.py
+++ khatma/routing.py
@@ -1,8 +1,8 @@
 from django_channels_jwt.urls import AsgiValidateTokenView
-from django.urls import re_path
+from django.urls import path
 from .consumers import *
 urlpatterns = [
-    re_path(r'chat/group/',khatmaGroupConsumer.as_asgi(),name='messaging the group'),
-    # re_path(r'/chat/brother',), for brothers only
+    path('chat/group/<int:group_id>/',khatmaGroupConsumer.as_asgi(),name='messaging the group'),
+    # re_path(r'/chat/broter',), for brothers only
     
 ] 
