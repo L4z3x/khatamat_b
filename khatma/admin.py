de47cb95e7fae87f0ad9297897dd0941e32ea11f
L4z3x <moussaousselmal1970@gmail.com>
2024-11-09 00:45:55 +0100

    added messages to admin panel

--- khatma/admin.py
+++ khatma/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from khatma.models import Khatma,khatmaMembership,khatmaGroup,khatmaGroupMembership
+from khatma.models import *
 # Register your models here.
 
 admin.site.register(Khatma)
 admin.site.register(khatmaMembership)
 admin.site.register(khatmaGroup)
 admin.site.register(khatmaGroupMembership)
+admin.site.register(message)
