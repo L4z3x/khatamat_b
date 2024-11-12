c4ff1d09af8539debcdf9bead4e0e78cfffa03bf
L4z3x <moussaousselmal1970@gmail.com>
2024-11-13 00:56:58 +0100

    updated

--- khatma/serializer.py
+++ khatma/serializer.py
@@ -3,28 +3,21 @@ from khatma.models import *
 
 
 class khatmaSerializer(serializers.ModelSerializer):
-    G_name = serializers.CharField(max_length=40)
     class Meta:
         model = Khatma
-        fields = ["name","startDate","endDate","G_name"]
-
+        fields = '__all__'
 
 class khatmaGroupSerializer(serializers.ModelSerializer):
     icon = serializers.ImageField(allow_empty_file=True,allow_null=True,required=False)
     class Meta:
         model = khatmaGroup
-        fields = ["name","icon"]
+        fields = ["id","name","icon"]
     
 
-class khatma_G_membSeriailizer(serializers.Serializer):
-    username = serializers.CharField(max_length=20)
-    G_name = serializers.CharField(max_length=40)
-
-
-
-class khatma_membSerializer(serializers.Serializer):
-    KH_name = serializers.CharField(max_length=20)
-    G_name = serializers.CharField(max_length=40)
+class khatma_membSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = khatmaMembership
+        fields = "__all__"
     
 class messageSerializer(serializers.ModelSerializer):
     class Meta:
--- khatma/urls.py
+++ khatma/urls.py
@@ -3,9 +3,10 @@ from khatma import views
 from django_channels_jwt.views import AsgiValidateTokenView
 urlpatterns = [
     path('auth/',AsgiValidateTokenView.as_view(),name='get uuid ticket'),
-    path("create/",views.CreateKhatma.as_view(),name="create khatma"),
-    path("retreive/<str:kh_name>/",views.getkhatma.as_view(),name="retreive khatma"),
-    path("create-khatma-group/",views.CreateKhatmaGroup.as_view(),name="create khatma group"),
-    path("add-member-kg/",views.khatma_G_memb.as_view(),name="add member to khatmaGroup"),
-    path("add-member-k/",views.khatma_memb.as_view(),name="add member to khatma"),
+    path("khatma/<int:id>/",views.khatma_details.as_view(),name="retreive or update khatma"),
+    path("khatma/",views.khatma_details.as_view(),name="create khatma"),
+    path("group/",views.Group.as_view(),name="create khatma group"),
+    path("add-member-group/<int:id>/",views.add_user_to_group,name="add member to khatmaGroup"),
+    path("khatma-membership/<int:id>/",views.khatma_membership.as_view(),name="add member to khatma"),
+    path("khatma-membership/",views.khatma_membership.as_view(),name="add member to khatma"),
 ]
