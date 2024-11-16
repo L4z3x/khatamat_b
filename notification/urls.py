6308e2d7d30e591c37ca504db6c34c25851edad4
L4z3x <moussaousselmal1970@gmail.com>
2024-11-16 09:33:58 +0100

    change variables in paths

--- notification/urls.py
+++ notification/urls.py
@@ -1,10 +1,8 @@
 from .views import * 
 from django.urls import path
 urlpatterns = [
-    path("send-join-req/<str:group_name>/", create_JoinRequest.as_view(), name="send join request"),
-    path("accept-join-req/<int:id>/",accept_joinReq,name="accept join request"),
-    # path("deny-join-req/<int:id>/",deny_joinReq,name="accept join request"), to be added
-    # path("list-join-req/<int:id>/",list_joinReq,name="accept join request"), to be added
+    path("send-join-req/<int:community_id>/",create_JoinRequest.as_view(),name='send community joinrequest(if authentication is not none)'),
+    path("accept-join-req/<int:sender_id/",accept_joinReq,name="accept join request to community"),
     path("send-br-req/<int:id>/",send_brothershipReq,name='send brothership request'),
     path("list-br-req/",list_brothershipReq,name='list brothership request'),
     path("accept-br-req/<int:id>/",accept_brothershipReq,name='accept brothership request'),
