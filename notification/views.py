a2ad128bb147dd27f4d286f8e4b0b8ee702c9974
L4z3x <moussaousselmal1970@gmail.com>
2024-11-16 09:29:00 +0100

    join request for community not for group

--- notification/views.py
+++ notification/views.py
@@ -1,99 +1,85 @@
-from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from api.models import MyUser,brothership
 from notification.models import joinRequest,brothershipRequest
 from rest_framework import views,status
 from rest_framework.decorators import api_view,permission_classes
-from khatma.models import khatmaGroup,khatmaGroupMembership
 from rest_framework.response import Response
 from .serializers import *
 from api.serializers import brotherDataSer
 from drf_spectacular.utils import extend_schema
+from community.models import communityMembership
 
 
 
-
-# tested and working >>> send multiple join requests to admins of a khatma group
+# if authentication of commuity is not none, send multiple join requests to admins of a community 
 class create_JoinRequest(views.APIView):
     permission_classes = [IsAuthenticated]
-    lookup_field = 'group_name'
+    lookup_field = 'community_id'
     
     def post(self,request,group_name,format=None): # to be updated more to handle more errors.
-        if group_name and len(group_name) < 50:
-            user = request.user
-            try:
-                khatmagroup = khatmaGroup.objects.get(name=group_name)
-            except:
-                return Response(data={"error":"khatma group not found"},status=status.HTTP_404_NOT_FOUND)
-            adminsMem = khatmagroup.khatma_G_membership.filter(role="admin")
-            print(adminsMem)
-            senders = adminsMem.values_list("user",flat=True)
-            print(senders)
-            admins =[]
-            for i in senders:
-                print(i)
-                try:
-                    sender = MyUser.objects.get(id=i)
-                    join_req = joinRequest.objects.get(sender=sender,receiver=user,khatmaGroup=khatmagroup)
-                except:
-                    join_req = None
-                if not join_req:
-                    joinrequest = joinRequest.objects.create(sender=sender,receiver=user,khatmaGroup=khatmagroup)
-                    joinrequest.save()
-                    admins.append(joinrequest.sender.username)
-                if not admins:
-                    return Response(data={"msg":"requset already sent"},status=status.HTTP_208_ALREADY_REPORTED)
-                else:
-                    print(admins)
-                    return Response(data={"msg":"request sent","admins number":f"{len(admins)}"},status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = request.user
+            
+        commu = community.objects.filter(name=group_name).first()
+
+        if not commu:
+            return Response(data={"error":"community not found"},status=status.HTTP_404_NOT_FOUND)
+        if commu.authentication == "none":
+            commu.members.add(user)
+            return Response(status=status.HTTP_202_ACCEPTED,data={"msg":f"added to {commu.name}"})
+        adminsMem = commu.membership.filter(role="admin").all()
+        admins =[]
+        for admin in adminsMem:
+            join_req = joinRequest.objects.filter(sender=user,receiver=admin.user,community=commu)
+
+            if not join_req:
+                joinrequest = joinRequest.objects.create(sender=user,receiver=admin.user,community=commu)
+                joinrequest.save()
+                admins.append(joinrequest.receiver.username)
+        if not admins:
+            return Response(data={"msg":"requset already sent"},status=status.HTTP_208_ALREADY_REPORTED)
+        else:
+            return Response(data={"msg":"request sent","admins number":f"{len(admins)}"},status=status.HTTP_201_CREATED)
     
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def send_brothershipReq(request,id):
     sender = request.user
-    if id:
-        try:
-            receiver = MyUser.objects.get(id=id)
-            if receiver == sender: # just for fun ;)
-                return Response(status=status.HTTP_406_NOT_ACCEPTABLE,data={"error":"user connot be brother with himself !!!"})
-            # check if they are brothers already
-            if brothership.objects.filter(user1=sender,user2=receiver).exists() or brothership.objects.filter(user1=receiver,user2=sender).exists():
-                return Response(data={"error":"you are already brothers"},status=status.HTTP_302_FOUND)
-            try:
-                br = brothershipRequest.objects.create(sender=sender,receiver=receiver,status='pending')
-                br.save()
-            except Exception as e:
-                return Response(status=status.HTTP_208_ALREADY_REPORTED,data={f"error":f"{e}"})
-        except:
-            return Response(data={"error":"this user doesn't exist"},status=status.HTTP_404_NOT_FOUND)
-        return Response(data={"msg":f"brothership Request sent to {receiver.username}"},status=status.HTTP_201_CREATED)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    receiver = MyUser.objects.filter(id=id).first()
+            
+    if receiver == sender: # just for fun ;)
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE,data={"error":"user connot be brother with himself !!!"})
+            
+        # check if they are brothers already
+    if brothership.objects.filter(user1=sender,user2=receiver).exists() or brothership.objects.filter(user1=receiver,user2=sender).exists():
+        return Response(data={"error":"you are already brothers"},status=status.HTTP_302_FOUND)
+            
+    br = brothershipRequest.objects.create(sender=sender,receiver=receiver,status='pending')
+    br.save()
+    return Response(data={"msg":f"brothership Request sent to {receiver.username}"},status=status.HTTP_201_CREATED)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def accept_brothershipReq(request,id):
     user = request.user
-    if id:
-        sender = MyUser.objects.get(id=id) # add whatever you want to send from the client side
-        print(sender.pk)
-        try:
-            br =  brothershipRequest.objects.get(sender=sender,receiver=user) # check for the brothership request
-            print(br)
-        except Exception as e:
-            return Response(data={"error":f"brothershipRequest not found ###{e}"},status= status.HTTP_404_NOT_FOUND)
-        if br:
-            if brothership.objects.filter(user1=sender,user2=user).exists() or brothership.objects.filter(user1=user,user2=sender).exists():
-                return Response(data={"error":"you are already brothers"},status=status.HTTP_302_FOUND)
+    
+    sender = MyUser.objects.filter(id=id).first()# add whatever you want to send from the client side
+    if not sender:
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"user not found"})
+    
+    if brothership.objects.filter(user1=sender,user2=user).exists() or brothership.objects.filter(user1=user,user2=sender).exists():
+        return Response(data={"error":"you are already brothers"},status=status.HTTP_302_FOUND)
+
+    br =  brothershipRequest.objects.filter(sender=sender,receiver=user).first() # check for the brothership request            return Response(data={"error":f"brothershipRequest not found ###{e}"},status= status.HTTP_404_NOT_FOUND)
+    if not br:
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"brothershiprequest not found"})
+        
+    br.status = "accepted"
+    br.save()
             
-            new_brothership = brothership.objects.create(user1=sender,user2=user) # create a new brothership
-            new_brothership.save()
-            br.status = "accepted"
-            br.save()
-            return Response(data={"msg":f"{sender.username} accepted"},status=status.HTTP_201_CREATED)
-        return Response(data={"error":f"brothershipRequest not found"},status= status.HTTP_404_NOT_FOUND)
-    return Response(status=status.HTTP_400_BAD_REQUEST,data={"error":"no id was passed with the url"})
+    new_brothership = brothership.objects.create(user1=sender,user2=user) # create a new brothership
+    new_brothership.save()
+    return Response(data={"msg":f"{sender.username} accepted"},status=status.HTTP_201_CREATED)
 
 
 @extend_schema(responses=brotherDataSer)
@@ -101,37 +87,37 @@ def accept_brothershipReq(request,id):
 @permission_classes([IsAuthenticated]) # /* add status to brothership request serialiazer and view *
 def list_brothershipReq(request):
     user = request.user
-    try:
-        req = brothershipRequest.objects.filter(sender=user,status='pending') | brothershipRequest.objects.filter(receiver=user,status='pending')
-        sen_data= []        
-        rec_data = []
-        for r in req:
-            receiver = r.sender
-            serializer = brotherDataSer(receiver).data
-            serializer['since']= r.created_at
-            serializer['id'] = r.pk
-            if receiver == user:
-                sen_data.append(serializer)
-            else:
-                rec_data.append(serializer)
-    except Exception as e:
-        return Response(data={"error":f"{e}"},status=status.HTTP_404_NOT_FOUND)
+    
+    req = brothershipRequest.objects.filter(sender=user,status='pending') | brothershipRequest.objects.filter(receiver=user,status='pending')
+    if not req:
+        return Response(data={"error":"no request found"},status=status.HTTP_404_NOT_FOUND)
+
+    sen_data= []        
+    rec_data = []
+    for r in req:
+        receiver = r.sender
+        serializer = brotherDataSer(receiver).data
+        serializer['since']= r.created_at
+        serializer['id'] = r.pk
+        if receiver == user:
+            sen_data.append(serializer)
+        else:
+            rec_data.append(serializer)
     return Response(data={"recieved":rec_data,"sent":sen_data},status=status.HTTP_200_OK)
     
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def deny_brothershipReq(request,id):
     user = request.user
-    try:
-        br = brothershipRequest.objects.get(id=id)
-    except:
+    br = brothershipRequest.objects.filter(id=id).first()
+    if not br:
         return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"no brothership request with associated with that id"})
     if br.receiver == user:
         br.status = 'rejected'
         br.save()
         return Response(status=status.HTTP_202_ACCEPTED,data={"msg":"the brothership req was rejected succesfully"})
     else:
-        return Response(status=status.HTTP_406_NOT_ACCEPTABLE,data={"msg":"the request was not meant for you"})
+        return Response(status=status.HTTP_403_FORBIDDEN,data={"msg":"the request was not meant for you"})
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -145,33 +131,30 @@ def accept_joinReq(request,id):
         name = serializer.validated_data['g_name']
 
     # check sender
-    try:
-        sender = MyUser.objects.get(id=id)
-    except:
+    sender = MyUser.objects.filter(id=id).first()
+    if not sender:
         return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"id of sender not found"})
 
     # check khatma group existance
-    try:
-        kh_group = khatmaGroup.objects.get(name=name)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"khatma group not found"})
+    commu = community.objects.get(name=name)
+    if not commu:
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"commu not found"})
 
     # check if user is admin of that khatma group 
-    if not khatmaGroupMembership.objects.filter(user=user,khatmaGroup=kh_group,role="admin").exists():    
-        return Response(status=status.HTTP_401_UNAUTHORIZED,data={"error":"your not admin of this khatma group"})
+    if not community.membership.objects.filter(user=user,group=commu,role="admin").exists():    
+        return Response(status=status.HTTP_401_UNAUTHORIZED,data={"error":"your not admin of this community"})
 
-    try:
-        join_req = joinRequest.objects.get(sender=sender,receiver=user,status="pending")
-    except:
-        if joinRequest.objects.filter(sender=sender,receiver=user,status="accepted").exists():
-            return Response(status=status.HTTP_302_FOUND,data={"msg":"join request already accepted"})
-        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"join request not found"})
+    if joinRequest.objects.filter(sender=sender,receiver=user,status="accepted").exists():
+        return Response(status=status.HTTP_302_FOUND,data={"msg":"join request already accepted"})
     
+    join_req = joinRequest.objects.filter(sender=sender,receiver=user,status="pending",community=commu).first()
+    if not join_req:
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"error":"join request not found"})
     join_req.status = "accepted"
     join_req.save()
     
-    new_kh_g_Membership = khatmaGroupMembership.objects.create(user=sender,role="user",khatmaGroup=kh_group)
-    new_kh_g_Membership.save()
+    new_commu_Membership = communityMembership.objects.create(user=sender,role="user",community=commu)
+    new_commu_Membership.save()
 
-    return Response(status=status.HTTP_202_ACCEPTED,data={"msg":f"accepted join request from {sender.username} to {kh_group.name}"})
+    return Response(status=status.HTTP_202_ACCEPTED,data={"msg":f"accepted join request from {sender.username} to {commu.name}"})
 
