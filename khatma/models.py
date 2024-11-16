1003fc86c1831bcf8f7aa6841d2497a94c73e372
L4z3x <moussaousselmal1970@gmail.com>
2024-11-16 09:26:58 +0100

    changed khatmaGroup to group

--- khatma/models.py
+++ khatma/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from api.models import MyUser
 
 
-class khatmaGroupManager(models.Manager):
-    def create_khatmaGroup(self,name,members=None,**extra):
+class groupManager(models.Manager):
+    def create_group(self,name,members=None,**extra):
         if not members:
             raise ValueError("expected members argument ??")
         group = self.create(name=name,**extra)
         group.members.set(members)
         return group
 
-class khatmaGroup(models.Model):  
+class group(models.Model):  
     name = models.CharField(max_length=40)
-    icon = models.ImageField(upload_to="khatmaGroupImages/",serialize=True)
-    members = models.ManyToManyField(MyUser,through="khatmaGroupMembership",related_name="khatmaGroup")
+    icon = models.ImageField(upload_to="groupImages/",serialize=True)
+    members = models.ManyToManyField(MyUser,through="groupMembership",related_name="group")
     description = models.CharField(max_length=290,default='BIO')    
-    objects = khatmaGroupManager()
+    objects = groupManager()
     
     def __str__(self):
         return self.name
@@ -23,12 +23,12 @@ class khatmaGroup(models.Model):
     def save(self,*args,**kwargs):
         super().save(*args,**kwargs)
         if not hasattr(self,"settings"):
-            settings = khatmaGroupSettings.objects.create(group=self)
+            settings = groupSettings.objects.create(group=self)
             self.settings = settings
             self.settings.save()        
             
             
-class khatmaGroupSettings(models.Model): # settings related to khatma
+class groupSettings(models.Model): # settings related to khatma
     MemberChoices = [
         ("all","all"),
         ("only_admins","only_admins"),
@@ -36,7 +36,7 @@ class khatmaGroupSettings(models.Model): # settings related to khatma
     ]
 
     # private = models.BooleanField(default=False,null=False)  i forgot why i added this field
-    group = models.OneToOneField(khatmaGroup,on_delete=models.CASCADE,related_name="settings")
+    group = models.OneToOneField(group,on_delete=models.CASCADE,related_name="settings")
     canAddMember = models.CharField(max_length=12,default="all",choices=MemberChoices)
     canAddMember_custom = models.ManyToManyField(MyUser,related_name="canAddMember")
     canSendMessage = models.CharField(max_length=12,default="all",choices=MemberChoices)
@@ -56,27 +56,27 @@ class khatmaGroupSettings(models.Model): # settings related to khatma
             if not user in self.group.members.all():
                 self.canAddMember_custom.remove(user)
             
-class khatmaGroupMembership(models.Model): # users inside a khatma group
+class groupMembership(models.Model): # users inside a khatma group
     ROLE = [
         ("admin","admin"),
         ("user","user"),
     ]
     since = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(MyUser,on_delete=models.CASCADE,related_name="khatmaGroupMembership")
-    khatmaGroup = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE,related_name="khatma_G_membership")
+    user = models.ForeignKey(MyUser,on_delete=models.CASCADE,related_name="groupMembership")
+    group = models.ForeignKey(group,on_delete=models.CASCADE,related_name="khatma_G_membership")
     role = models.CharField(default="admin",max_length=6,choices=ROLE,blank=False)
     def __str__(self):
-        return f"{self.user} in {self.khatmaGroup} group"
+        return f"{self.user} in {self.group} group"
 
 
 class media(models.Model):
-    group = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE,related_name="media")
-    sender = models.ForeignKey(khatmaGroupMembership,on_delete=models.SET_NULL,null=True)
-    image = models.ImageField(upload_to="khatmaGroupMedia/")
+    group = models.ForeignKey(group,on_delete=models.CASCADE,related_name="media")
+    sender = models.ForeignKey(groupMembership,on_delete=models.SET_NULL,null=True)
+    image = models.ImageField(upload_to="groupMedia/")
     
 class message(models.Model):
-    sender = models.ForeignKey(khatmaGroupMembership,on_delete=models.CASCADE)
-    group = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE)
+    sender = models.ForeignKey(groupMembership,on_delete=models.CASCADE)
+    group = models.ForeignKey(group,on_delete=models.CASCADE)
     message = models.TextField(max_length=400000,null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -87,12 +87,12 @@ class message(models.Model):
 # khatma section:
       
 class KhatmaManager(models.Manager): # khatma model manager (create,delet,update,list)
-    def create_khatma(self, name, khatmaGroup,period):
-        if khatmaGroup:
-            khatma = self.model(name=name,period=period,khatmaGroup=khatmaGroup)
+    def create_khatma(self, name, group,period):
+        if group:
+            khatma = self.model(name=name,period=period,group=group)
             khatma.save()
             return khatma
-        raise ValueError("expected khatmaGroup argument ??")
+        raise ValueError("expected group argument ??")
 
     def delete_khatma(self, name):
         khatma = self.get(name=name)
@@ -101,7 +101,7 @@ class KhatmaManager(models.Manager): # khatma model manager (create,delet,update
     
     def list_khatma_member(self,name):
         khatma = self.get(name=name)
-        return khatma.khatmaGroup.members.all()
+        return khatma.group.members.all()
 
 SurahList = [
         ("the cow","the cow"),
@@ -113,11 +113,11 @@ KhatmaStatusList = [
         ("aborted","aborted"),
 ]
 
-class Khatma(models.Model):    # khatma instance created by a khatmaGroup Admin
+class Khatma(models.Model):    # khatma instance created by a group Admin
     
     launcher = models.ForeignKey(MyUser,on_delete=models.SET_NULL,null=True,related_name="launched_khatmas")  
     name = models.CharField(max_length=20,null=False)
-    khatmaGroup = models.ForeignKey(khatmaGroup,on_delete=models.CASCADE,null=False,default=None)
+    group = models.ForeignKey(group,on_delete=models.CASCADE,null=False,default=None)
     endDate = models.DateTimeField(null=False)
     intentions = models.CharField(max_length=180,default=None) # required , not null provide choices
     duaa = models.CharField(max_length=180,default=None) # required , not null provide choices
@@ -127,16 +127,16 @@ class Khatma(models.Model):    # khatma instance created by a khatmaGroup Admin
     endVerse = models.PositiveIntegerField(null=False,default=0)
     
     progress = models.PositiveIntegerField(default=0,null=False)
-
+    status = models.CharField(choices=KhatmaStatusList,max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = KhatmaManager()
     
     class Meta:
-        unique_together = [("name","khatmaGroup")]
+        unique_together = [("name","group")]
         
     def __str__(self):
-        return f"{self.name} in {self.khatmaGroup}"
+        return f"{self.name} in {self.group}"
     def save(self,*args,**kwargs):
         super().save(*args,**kwargs)
         total_membership = 0
@@ -144,12 +144,13 @@ class Khatma(models.Model):    # khatma instance created by a khatmaGroup Admin
         for membership in khatmaMembership.objects.filter(khatma=self):
             total_progress += membership.progress
             total_membership += 1
-        self.progress = total_progress / total_membership
-        super().save(*args,**kwargs)
+        if total_membership != 0:
+            self.progress = int(total_progress / total_membership)
+            super().save(*args,**kwargs)
         
 class khatmaMembership(models.Model):
     
-    khatmaGroupMembership = models.ForeignKey(khatmaGroupMembership,on_delete=models.CASCADE,null=False,related_name="khatmaMembership")   
+    groupMembership = models.ForeignKey(groupMembership,on_delete=models.CASCADE,null=False,related_name="khatmaMembership")   
     khatma = models.ForeignKey(Khatma,on_delete=models.CASCADE,null=False,default=None)
    
     startShareSurah = models.CharField(max_length=35,choices=SurahList)
@@ -165,10 +166,10 @@ class khatmaMembership(models.Model):
     status = models.CharField(max_length=20,default="ongoing",choices=KhatmaStatusList)
     
     class Meta:
-        unique_together = [("khatma","khatmaGroupMembership")]
+        unique_together = [("khatma","groupMembership")]
     
     def __str__(self):
-        name = f"{self.khatmaGroupMembership} in {self.khatma.name}" 
+        name = f"{self.groupMembership} in {self.khatma.name}" 
         return name
 
     def save(self,*args,**kwargs):            
