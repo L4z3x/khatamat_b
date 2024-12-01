736aa04d7a11c0b12b634ee3fcec02fb3dbff049
L4z3x <moussaousselmal1970@gmail.com>
2024-12-01 15:35:03 +0100

    avoided infinit loop

--- app/api/signals.py
+++ app/api/signals.py
@@ -6,5 +6,16 @@ from .models import MyUser
 def update_brothers_num(sender, instance, **kwargs):
     if kwargs.get('created', False):
         return  # Skip if the user is being created
-    instance.brothersNum = len(set(instance.brothers.all() | instance.brothers_set.all()))
-    instance.save(update_fields=['brothersNum'])
+
+    # Avoid recursion by using a flag
+    if getattr(instance, '_updating_brothers_num', False):
+        return
+
+    # Set the flag to avoid recursion
+    instance._updating_brothers_num = True
+    try:
+        instance.brothersNum = len(set(instance.brothers.all() | instance.brothers_set.all()))
+        instance.save(update_fields=['brothersNum'])
+    finally:
+        # Ensure the flag is reset
+        instance._updating_brothers_num = False
