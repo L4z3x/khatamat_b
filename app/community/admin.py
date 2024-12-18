64354df2dc1fdece2dd621ea8841e08fd88b9752
L4z3x <moussaousselmal1970@gmail.com>
2024-12-18 08:11:30 +0100

    removed community app

--- app/community/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(community)
-admin.site.register(communityMembership)
-admin.site.register(post)
-admin.site.register(comment)
